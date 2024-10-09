83af14c24dbd832eaff03f325b964352f0125974
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-10-09 11:17:24 +0700

    add data education

---
+++ lib/data/education.ts
@@ -0,0 +1,29 @@
+import { Education } from "../types";
+
+export const EDUCATION: Education[] = [
+  {
+    address: "Surakarta, Jawa Tengah, Indonesia",
+    fromYear: 2018,
+    endYear: 2022,
+    logoUrl: "/svgs/logo-udb-text.png",
+    schoolFullName: "Universitas Duta Bangsa Surakarta",
+    schoolLabel: "UDB",
+    score: 3.4,
+    scoreMax: 4.0,
+    level: 'Bachelor\'s degree',
+    major: 'Computer Engineering, (S.T)',
+  },
+  {
+    address: "Surakarta, Jawa Tengah, Indonesia",
+    fromYear: 2015,
+    endYear: 2018,
+    logoUrl: "/svgs/logo-yosef.png",
+    schoolFullName: "SMA PL Santo Yosef Surakarta",
+    schoolLabel: "SMA Yosef",
+    score: 6.7,
+    scoreMax: 10,
+    level: 'Student',
+    major: null,
+  },
+
+]
