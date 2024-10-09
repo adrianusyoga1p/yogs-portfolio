0bf5ff81faa1459e074a853a65705a08b716c4b6
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-10-09 11:13:48 +0700

    add card about

---
+++ app/(views)/about/_components/card-about.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface CardProps {
+  children: React.ReactNode;
+}
+
+const CardAbout = ({ children }: CardProps) => {
+  return (
+    <div className="p-6 rounded-lg border border-cborder">
+      <div className="flex max-sm:flex-col gap-6 sm:items-center">{children}</div>
+    </div>
+  );
+};
+
+const CardImage = ({ children }: CardProps) => {
+  return <div className="max-w-24">{children}</div>;
+};
+
+const CardDetail = ({ children }: CardProps) => {
+  return <div className="space-y-4">{children}</div>;
+};
+
+export { CardAbout, CardImage, CardDetail };
