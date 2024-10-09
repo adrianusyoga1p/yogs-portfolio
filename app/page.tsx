3f737565328d531bc67dab7a11924de7b300e349
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-10-09 11:13:31 +0700

    add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,52 @@
-import Image from "next/image";
+import BadgeTech from "@/components/ui/badge-tech";
+import Card from "@/components/ui/card";
+import CarouselCard from "@/components/ui/carousel-card";
+import { PROJECT, STACKS } from "@/lib/data";
+import { PROFILE } from "@/lib/data/profile";
+import { Laptop, MapPin } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <section className="space-y-4">
+        <h1 className="text-foreground text-xl font-bold">
+          Hi, I am {PROFILE.name} as a Frontend Developer
+        </h1>
+        <div className="flex max-sm:flex-col sm:items-center sm:gap-6 gap-2 text-gray-400">
+          <div className="flex gap-2 items-center">
+            <MapPin className="w-4 h-4" />
+            <p className="text-sm">Based in {PROFILE.address}</p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <Laptop className="w-4 h-4" />
+            <p className="text-sm">{PROFILE.statusTwo}</p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <p className="text-foreground leading-relaxed">{PROFILE.description}</p>
+      </section>
+      <section>
+        <h1 className="text-foreground text-lg font-semibold">
+          Technology I&apos;ve Used
+        </h1>
+        <div className="relative overflow-hidden min-h-10 mt-6">
+          <div className="absolute left-0 from-background bg-gradient-to-r md:w-60 w-32 h-full z-20 top-0"></div>
+          <div className="absolute right-0 from-background bg-gradient-to-l md:w-60 w-32 h-full z-20 top-0"></div>
+          <div className="space-x-4 whitespace-nowrap overflow-x-scroll scroll-hide animation-slide absolute left-0 h-full">
+            {STACKS.map((stack, i) => (
+              <BadgeTech data={stack} key={i} />
+            ))}
+          </div>
+        </div>
+      </section>
+      <section>
+        <CarouselCard title="My Projects" data={PROJECT}>
+          {PROJECT.filter((data) => data.id)
+            .sort((a, b) => a.id - b.id)
+            .map((data) => (
+              <Card data={data} key={data.id} />
+            ))}
+        </CarouselCard>
+      </section>
+    </>
   );
 }
