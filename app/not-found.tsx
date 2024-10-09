4fc42e9fb55a3640884986994656731e31979838
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-10-09 11:07:29 +0700

    git init

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div className='bg-background text-center min-h-[80vh] flex items-center justify-center'>
+      <h1 className="text-2xl">Page is not found</h1>
+    </div>
+  )
+}
+
+export default NotFoundPage
