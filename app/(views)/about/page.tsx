8053a54a650d4833b5b220e4443aeccd718f0c79
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-10-09 15:40:50 +0700

    add resume content

--- app/(views)/about/page.tsx
+++ app/(views)/about/page.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import { BriefcaseBusiness, School } from "lucide-react";
+import { BriefcaseBusiness, Link, School } from "lucide-react";
 import React from "react";
 import { Tabs } from "@/components/ui/tab";
 import EducationContent from "./_components/education-content";
@@ -10,14 +8,24 @@ const AboutPage = () => {
   return (
     <section className="space-y-4">
       <h1 className="font-bold text-xl">About</h1>
+      <p className="text-gray-400">
+        An insightful glimpse into who I am - because every detail adds depth to
+        the canvas of life.
+      </p>
       <Tabs
         headers={[
+          { label: "Resume", icon: <Link size={16} /> },
           { label: "Education", icon: <School size={16} /> },
           { label: "Works Experience", icon: <BriefcaseBusiness size={16} /> },
         ]}
       >
-        <EducationContent/>
-        <WorkContent/>
+        <iframe
+          src="https://drive.google.com/file/d/15rdq6i0-s4zdskm19kTZZ23j8vRVCFdF/preview"
+          className='w-full h-dvh'
+          loading='lazy'
+        />
+        <EducationContent />
+        <WorkContent />
       </Tabs>
     </section>
   );
