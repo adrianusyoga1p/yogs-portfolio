ece6056e6dc734e62b1fc1354ce95502b9b28108
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-10-09 11:15:09 +0700

    add project component

---
+++ app/(views)/project/page.tsx
@@ -0,0 +1,32 @@
+import Card from "@/components/ui/card";
+import { PROJECT } from "@/lib/data";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Adrians Projects",
+  description: "My projects",
+};
+
+const ProjectsPage = () => {
+  return (
+    <section className="space-y-4">
+      <h1 className="text-foreground text-xl font-bold">Projects</h1>
+      <p className="text-gray-400">
+        Several projects that I have worked on, both private and open source.
+      </p>
+      <div className="grid xl:grid-cols-3 md:grid-cols-2 gap-6">
+        {PROJECT.filter((data) => data.id)
+          .sort((a, b) => a.id - b.id)
+          .map((data) => (
+            <Card
+              className="sm:max-w-full w-full"
+              data={data}
+              key={data.id}
+            />
+          ))}
+      </div>
+    </section>
+  );
+};
+
+export default ProjectsPage;
