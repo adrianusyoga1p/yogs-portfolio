a335091637410d02a7de8d3eca9d1848e803654c
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-10-10 15:49:16 +0700

    adjust badge stack

--- components/ui/badge-tech.tsx
+++ components/ui/badge-tech.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const BadgeTech = ({ data }: { data: Stack }) => {
   return (
-    <div className="inline-block rounded-full w-fit whitespace-normal border border-cborder py-2 px-3">
+    <div className="rounded-full inline-block whitespace-normal w-fit border border-cborder py-2 px-3">
       <div className="flex gap-3 items-center text-sm">
         <Image width={20} height={20} src={data.imageUrl} alt="icon" />
         {data.label}
