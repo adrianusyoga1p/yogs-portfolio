a19dd71ba08c76884c7b09663f91c651a6b0c877
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-10-10 11:51:40 +0700

    add resume content

---
+++ app/(views)/about/_components/resume-content.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ResumeContent = () => {
+  return (
+    <div className="m-0 p-0 h-screen w-full">
+      <iframe
+        src="/docs/Resume_Adrianus_Yoga_Prasetyo.pdf"
+        className="w-full h-dvh border-none"
+        loading="lazy"
+        title="Resume PDF"
+      />
+    </div>
+  );
+};
+
+export default ResumeContent;
