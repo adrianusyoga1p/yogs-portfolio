5c4439e6e8d17ad7f5288c4361ff4e5f59acd971
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-10-10 12:03:11 +0700

    adjust project page

--- app/(views)/project/page.tsx
+++ app/(views)/project/page.tsx
@@ -9,23 +9,21 @@ export const metadata: Metadata = {
 
 const ProjectsPage = () => {
   return (
-    <section className="space-y-4">
-      <h1 className="text-foreground text-xl font-bold">Projects</h1>
-      <p className="text-gray-400">
-        Several projects that I have worked on, both private and open source.
-      </p>
-      <div className="grid xl:grid-cols-3 md:grid-cols-2 gap-6">
+    <>
+      <section className="space-y-4">
+        <h1 className="text-foreground text-xl font-bold">Projects</h1>
+        <p className="text-gray-400">
+          Several projects that I have worked on, both private and open source.
+        </p>
+      </section>
+      <section className="grid xl:grid-cols-3 md:grid-cols-2 gap-6">
         {PROJECT.filter((data) => data.id)
           .sort((a, b) => a.id - b.id)
           .map((data) => (
-            <Card
-              className="sm:max-w-full w-full"
-              data={data}
-              key={data.id}
-            />
+            <Card className="sm:max-w-full w-full" data={data} key={data.id} />
           ))}
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
