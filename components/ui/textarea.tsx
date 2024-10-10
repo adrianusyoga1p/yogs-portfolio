d5eaa43f8666a5b31408af103b09a8e07b98501d
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-10-10 12:03:56 +0700

    add textarea component from radix

---
+++ components/ui/textarea.tsx
@@ -0,0 +1,24 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          "flex min-h-[100px] w-full rounded-md border border-input bg-background p-3 text-sm ring-offset-background placeholder:text-gray-400 ring-0 outline-0 disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Textarea.displayName = "Textarea"
+
+export { Textarea }
