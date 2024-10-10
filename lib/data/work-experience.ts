a8215bfd81ddbf7c11b0d428f1a71d12755d69c0
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-10-10 15:56:06 +0700

    adjust data work experience

--- lib/data/work-experience.ts
+++ lib/data/work-experience.ts
@@ -3,21 +3,6 @@ import { Work } from "../types";
 export const WORK: Work[] = [
   {
     address: "Surakarta, Central Java, Indonesia",
-    companyFullName: "Duxeos Software House",
-    companyLabel: "Duxeos",
-    fromDate: "November 2020",
-    endDate: "January 2021",
-    role: "Frontend Developer",
-    workType: "On-site",
-    workMode: "Internship",
-    logoUrl: "/svgs/logo-duxeos.webp",
-    responsibilities: [
-      'Play a role in developing the appearance of the website.',
-      'Study system design and UI/UX design.',
-    ]
-  },
-  {
-    address: "Surakarta, Central Java, Indonesia",
     companyFullName: "Phicos Cipta Media",
     companyLabel: "Phicos",
     fromDate: "January 2023",
@@ -30,6 +15,21 @@ export const WORK: Work[] = [
       'Developing the front appearance of a website, using front-end technology.',
       'Presenting project results to clients.',
       'Create an initial design for the appearance of the website.',
+    ]
+  },
+  {
+    address: "Surakarta, Central Java, Indonesia",
+    companyFullName: "Duxeos Software House",
+    companyLabel: "Duxeos",
+    fromDate: "November 2020",
+    endDate: "January 2021",
+    role: "Frontend Developer",
+    workType: "On-site",
+    workMode: "Internship",
+    logoUrl: "/svgs/logo-duxeos.webp",
+    responsibilities: [
+      'Play a role in developing the appearance of the website.',
+      'Study system design and UI/UX design.',
     ]
   },
 ]
