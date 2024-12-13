b6202558c28f4e9bff02be8cf1712775efd2301c
adrianusyoga1p <adrianusyoga26@gmail.com>
2024-12-13 22:09:02 +0700

    change photo profile

--- lib/data/profile.ts
+++ lib/data/profile.ts
@@ -8,7 +8,7 @@ export const PROFILE: Profile = {
   address: 'East Jakarta, Indonesia',
   statusOne: 'Currently work at Mariworks',
   statusTwo: 'Available for freelance',
-  avatar: 'https://media.licdn.com/dms/image/v2/D5603AQFHxVHoq5lrYw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1721292147003?e=1731542400&v=beta&t=zGNZr-1wbBCI0GHOL8VBVaA8wcYGcNK8BBz6lMsyGo0',
+  avatar: '/public/images/avatar-profile.jpeg',
   description: 'I am a Frontend Web Developer with 2 years of experience, skilled in building modern and responsive web interfaces using various tools such as Bootstrap, Tailwind CSS, React, Vue, and Next.js. With strong proficiency in JavaScript, state management using Redux, and backend integration with Firebase, I can deliver efficient and scalable web applications. My educational background as a Bachelor of Computer Science from Universitas Duta Bangsa Surakarta also provides me with a solid technical foundation to tackle challenges in web development.',
   socials: [
     {
