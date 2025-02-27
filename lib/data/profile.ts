6c599c505a3b780178be309e1485a2d1e9eb659b
adrianusyoga1p <adrianusyoga26@gmail.com>
2025-02-27 10:39:39 +0700

    fix: dynamic work experience

--- lib/data/profile.ts
+++ lib/data/profile.ts
@@ -2,30 +2,33 @@ import { Github, Instagram, Linkedin } from "lucide-react";
 import { Profile } from "../types";
 import { STACKS } from "./stacks";
 
+const startWork: number = 2023;
+const experienceYear: number = new Date().getFullYear() - startWork;
+
 export const PROFILE: Profile = {
-  name: 'Adrianus Yoga Prasetyo',
-  sureName: 'Adrian',
-  address: 'East Jakarta, Indonesia',
-  statusOne: 'Currently work at Mariworks',
-  statusTwo: 'Available for freelance',
-  avatar: 'https://github.com/adrianusyoga1p.png',
-  description: 'I am a Frontend Web Developer with 2 years of experience, skilled in building modern and responsive web interfaces using various tools such as Bootstrap, Tailwind CSS, React, Vue, and Next.js. With strong proficiency in JavaScript, state management using Redux, and backend integration with Firebase, I can deliver efficient and scalable web applications. My educational background as a Bachelor of Computer Science from Universitas Duta Bangsa Surakarta also provides me with a solid technical foundation to tackle challenges in web development.',
+  name: "Adrianus Yoga Prasetyo",
+  sureName: "Adrian",
+  address: "East Jakarta, Indonesia",
+  statusOne: "Currently work at TixFly part of MWS",
+  statusTwo: "Available for freelance",
+  avatar: "https://github.com/adrianusyoga1p.png",
+  description: `I am a Frontend Web Developer with ${experienceYear} years of experience, skilled in building modern and responsive web interfaces using various tools such as Bootstrap, Tailwind CSS, React, Vue, and Next.js. With strong proficiency in JavaScript, state management using Redux, and backend integration with Firebase, I can deliver efficient and scalable web applications. My educational background as a Bachelor of Computer Science from Universitas Duta Bangsa Surakarta also provides me with a solid technical foundation to tackle challenges in web development.`,
   socials: [
     {
       label: "Github",
-      href: 'https://github.com/adrianusyoga1p',
-      icon: Github
+      href: "https://github.com/adrianusyoga1p",
+      icon: Github,
     },
     {
       label: "Linkedin",
-      href: 'https://www.linkedin.com/in/adrianus-yoga-prasetyo-244388196/',
-      icon: Linkedin
+      href: "https://www.linkedin.com/in/adrianus-yoga-prasetyo-244388196/",
+      icon: Linkedin,
     },
     {
       label: "Instagram",
-      href: 'https://www.instagram.com/adrianusyoga_?igsh=c3owb2xoeDQ5MWRy',
-      icon: Instagram
+      href: "https://www.instagram.com/adrianusyoga_?igsh=c3owb2xoeDQ5MWRy",
+      icon: Instagram,
     },
   ],
-  skills: STACKS
-}
+  skills: STACKS,
+};
